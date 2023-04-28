5880208fa7f55e5af4d41a6c025fb4a58ff40735
DanTimoshenko <113703529+DanTimoshenko@users.noreply.github.com>
2023-04-28 10:28:07 +0300

    Update 6laba.sql

--- 6laba.sql
+++ 6laba.sql
@@ -14,42 +14,41 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-SET NAMES utf8mb4;
+DROP TABLE IF EXISTS `Line_of_metro`;
+CREATE TABLE `Line_of_metro` (
+  `id` int(255) NOT NULL,
+  `name` char(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS `branches_of_metro`;
-CREATE TABLE `branches_of_metro` (
-  `name_of_branch` char(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `Name_of_city` char(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `Name_of_country` char(50) COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+INSERT INTO `Line_of_metro` (`id`, `name`) VALUES
+(1,	'green'),
+(2,	'red'),
+(3,	'blue');
 
+DROP TABLE IF EXISTS `station_of_metro`;
+CREATE TABLE `station_of_metro` (
+  `id_of_station` int(255) NOT NULL,
+  `name_of_station` char(255) NOT NULL,
+  `number_of_line` int(255) NOT NULL,
+  `number_of_station_in_line` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS `Cities_with_metro`;
-CREATE TABLE `Cities_with_metro` (
-  `Id` int(100) NOT NULL AUTO_INCREMENT,
-  `Cities_with_metro` char(30) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `Country` char(30) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+INSERT INTO `station_of_metro` (`id_of_station`, `name_of_station`, `number_of_line`, `number_of_station_in_line`) VALUES
+(1,	'HolodnaGora',	2,	1),
+(2,	'GeroevPrasi',	3,	1),
+(3,	'Peremoga',	1,	1),
+(4,	'Indystialna',	2,	13),
+(5,	'IstorishniiMysei',	3,	8),
+(6,	'MetroBudivnikiv',	1,	9);
 
-INSERT INTO `Cities_with_metro` (`Id`, `Cities_with_metro`, `Country`) VALUES
-(1,	'Kharkiv',	'Ukraine'),
-(2,	'Kiev',	'Ukraine'),
-(3,	'Palma',	'Italy');
+DROP TABLE IF EXISTS `tranisition_on_line_to_line`;
+CREATE TABLE `tranisition_on_line_to_line` (
+  `from_line` int(255) NOT NULL,
+  `to_line` int(255) NOT NULL,
+  `from_station` int(255) NOT NULL,
+  `to_station` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS `places`;
-CREATE TABLE `places` (
-  `name` char(30) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `branch` char(30) COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-
-DROP TABLE IF EXISTS `Transition stations`;
-CREATE TABLE `Transition stations` (
-  `Зі станції` char(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `На станцію` char(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `З гілки` char(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `На гілку` char(50) COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
+INSERT INTO `tranisition_on_line_to_line` (`from_line`, `to_line`, `from_station`, `to_station`) VALUES
+(2,	3,	4,	1);
 
