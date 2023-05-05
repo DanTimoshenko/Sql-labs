bf193e44d26aca3f07cd9c92ca51026721f75a05
DanTimoshenko <113703529+DanTimoshenko@users.noreply.github.com>
2023-05-06 00:09:50 +0300

    Update 7laba.sql

--- 7laba.sql
+++ 7laba.sql
@@ -1,28 +1,9 @@
--- Adminer 4.8.1 MySQL 5.5.5-10.3.32-MariaDB-1:10.3.32+maria~focal dump
+Adminer 4.8.1 MySQL 5.5.5-10.3.32-MariaDB-1:10.3.32+maria~focal dump
 
 SET NAMES utf8;
 SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
-
-DROP TABLE IF EXISTS `news`;
-CREATE TABLE `news` (
-  `id` int(255) NOT NULL,
-  `id_of_news` int(255) NOT NULL,
-  `name` char(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
-DROP TABLE IF EXISTS `rating`;
-CREATE TABLE `rating` (
-  `ip` int(255) NOT NULL,
-  `id_of_news` int(255) NOT NULL,
-  `comment` char(255) NOT NULL,
-  `rating_of_news` int(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-INSERT INTO `rating` (`ip`, `id_of_news`, `comment`, `rating_of_news`) VALUES
-(1,	1,	'I was waiting more from Vadim',	3);
 
 DROP TABLE IF EXISTS `registration`;
 CREATE TABLE `registration` (
@@ -31,6 +12,9 @@ CREATE TABLE `registration` (
   `email` char(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+INSERT INTO `registration` (`ip`, `nickname`, `email`) VALUES
+(1,	'Danchick',	'123253263g@gmail.com'),
+(2,	'AndriiSergiovich',	'andrii.khoroshun@khpi.edu.ua');
 
 DROP TABLE IF EXISTS `topic`;
 CREATE TABLE `topic` (
@@ -38,5 +22,48 @@ CREATE TABLE `topic` (
   `topic_of_news` char(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+INSERT INTO `topic` (`id`, `topic_of_news`) VALUES
+(1,	'sport'),
+(2,	'nature'),
+(3,	'science');
 
--- 2023-05-05 20:25:31
+DROP TABLE IF EXISTS `news`;
+CREATE TABLE `news` (
+  `id` int(255) NOT NULL,
+  `id_of_news` int(255) NOT NULL,
+  `name` char(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `news` (`id`, `id_of_news`, `name`) VALUES
+(1,	1,	'Match of the Year: Daniil Timoshenko vs Vadim Plushnikov'),
+(2,	1,	'Scientists have discovered an immortal animal'),
+(3,	1,	'Physicists have discovered a new property of gravity');
+
+DROP TABLE IF EXISTS `rating`;
+CREATE TABLE `rating` (
+  `ip` int(255) NOT NULL,
+  `id_of_news` int(255) NOT NULL,
+  `comment` char(255) NOT NULL,
+  `rating_of_news` int(255) NOT NULL,
+  KEY `rating_of_news` (`rating_of_news`),
+  CONSTRAINT `rating_ibfk_1` FOREIGN KEY (`rating_of_news`) REFERENCES `numbers` (`rating_of_news`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `rating` (`ip`, `id_of_news`, `comment`, `rating_of_news`) VALUES
+(1,	1,	'I was waiting more from Vadim',	2);
+
+DROP TABLE IF EXISTS `numbers`;
+CREATE TABLE `numbers` (
+  `rating_of_news` int(255) NOT NULL,
+  PRIMARY KEY (`rating_of_news`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `numbers` (`rating_of_news`) VALUES
+(1),
+(2),
+(3),
+(4),
+(5);
+
+
+-- 2023-05-05 21:04:23
