2a521b92845b7f04fd3ad7959abc925d8c86da4c
DanTimoshenko <113703529+DanTimoshenko@users.noreply.github.com>
2023-05-06 10:36:51 +0300

    Update 7laba.sql

--- 7laba.sql
+++ 7laba.sql
@@ -22,7 +22,7 @@ CREATE TABLE `topic` (
   `topic_of_news` char(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `topic` (`id`, `topic_of_news`) VALUES
+INSERT INTO `topics` (`id`, `topic_of_news`) VALUES
 (1,	'sport'),
 (2,	'nature'),
 (3,	'science');
@@ -30,14 +30,14 @@ INSERT INTO `topic` (`id`, `topic_of_news`) VALUES
 DROP TABLE IF EXISTS `news`;
 CREATE TABLE `news` (
   `id` int(255) NOT NULL,
-  `id_of_news` int(255) NOT NULL,
+  `topic_id` int(255) NOT NULL,
   `name` char(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO `news` (`id`, `id_of_news`, `name`) VALUES
 (1,	1,	'Match of the Year: Daniil Timoshenko vs Vadim Plushnikov'),
-(2,	1,	'Scientists have discovered an immortal animal'),
-(3,	1,	'Physicists have discovered a new property of gravity');
+(1,	2,	'Scientists have discovered an immortal animal'),
+(1,	3,	'Physicists have discovered a new property of gravity');
 
 DROP TABLE IF EXISTS `rating`;
 CREATE TABLE `rating` (
